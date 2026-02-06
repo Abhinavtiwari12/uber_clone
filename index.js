3703de54d60ae7e1f0eb7f136b232fc834f843d9
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-06 23:51:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// require ('dotenv').config()
+
+import dotenv from 'dotenv'
+
+import express from "express";
+import connectDB from "./database/db.js";
+
+import { app } from './app.js'
+
+dotenv.config({
+    path: '.env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+        console.log(`server is running on port: ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("mongoose connectioon failed", err)
+})
