502ad1a7c9ebdefd82c133ea23dd4c7fc2092253
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-07 21:44:14 +0530

    Create user Schema and bcrypt

---
+++ src/models/user.model.js
@@ -0,0 +1,74 @@
+import express from "express";
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt, { sign } from "jsonwebtoken";
+import { use } from "react";
+
+
+const userSchema = new Schema ({
+    fullName:{
+        type: String,
+        require: true,
+        index: true
+    },
+    userName:{
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true      
+    },
+    password:{
+        type: String,
+        require: true,
+        select: false
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true 
+    },
+    refreshToken:{
+        type: String
+    },
+},{timestamps: true}
+)
+
+
+userSchema.pre("save", async function () {
+    if(!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        userName: this.userName,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign( {
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
