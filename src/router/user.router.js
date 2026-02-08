3c84d4962ac2bb3b0328a87f41c2db14649f9c74
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-08 23:22:09 +0530

    create router for register route

---
+++ src/router/user.router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { registerUser } from "../controller/user.controller";
+
+
+
+const router = Router()
+
+
+router.route('/registerUser').post(registerUser)
+
+
+export default router
