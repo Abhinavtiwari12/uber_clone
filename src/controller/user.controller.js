52370473e1141ac9f056ed9af19810031494b25d
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-08 23:41:34 +0530

    create user login API

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -6,6 +6,10 @@ import { apiResponse } from "../utils/apiResponse";
 import { User } from "../models/user.model";
 import { findUser, registerUser } from "../service/user.service";
 
+
+
+
+
 const generateAccesaAndRefreshToken = async(userId) => {
    try {
      const user = await User.findById(userId)
@@ -52,7 +56,40 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 
+const loginUser = asyncHandler(async (req, res) => {
 
+    const {email, userNmae, password} = req.body
+
+    if (!userNmae && !email) {
+        throw new ApiError(401, "Username or email is require.")
+    }
+
+    const user = await User.findOne({
+        $or: [{userNmae}, {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(400, "username, email or password is wrong.")
+    }
+
+    const checkPassword = await user.isPasswordCorrect(password)
+
+    if (!checkPassword) {
+        throw new ApiError(400, "username, email or password is wrong.")
+    }
+
+    const {accesToken, refreshToken} = await generateAccesaAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken -accessToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options)
+    .json(new apiResponse(200, { user: loggedInUser, accessToken, refreshToken}, "user loggedIn successfull."))
+})
 
 
 
