1cf2b2faf90629d46c651dd95862a3581d506a01
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-09 12:30:41 +0530

    fix error

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser, userlogout } from "../controller/user.controller";
+import { loginUser, createUser, userlogout } from "../controller/user.controller.js";
 
 
 
 const router = Router()
 
 
-router.route('/registerUser').post(registerUser)
+router.route('/registerUser').post(createUser)
 router.route('/userlogin').post(loginUser)
 router.route('/userlogout').post(userlogout)
 
 
+// export default router
 export default router
