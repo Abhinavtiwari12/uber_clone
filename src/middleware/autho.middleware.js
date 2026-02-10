320256df30e70e9310a2df4840cde37cc4a066e2
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-10 22:38:14 +0530

    add driver verification in middleware

--- src/middleware/autho.middleware.js
+++ src/middleware/autho.middleware.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
+import { driver } from "../models/driver.model.js";
 
 
 
@@ -24,6 +25,31 @@ import { User } from "../models/user.model.js";
         }
 
         req.user = user;
+        next()
+
+    } catch (error) {
+        throw new ApiError(400, "invilad token access.")
+    }
+})
+
+
+ const verifyDriverjwt = asyncHandler( async (req, res, next) => {
+    try {
+
+        const token = req.cookies?.accessToken  ||  req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!token) {
+            throw new ApiError(401, "unathorized access")
+        }
+
+        const decodedToken =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const driver = await driver.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if (!driver) {
+            throw new ApiError(401, "invilad access token.")
+        }
+
+        req.driver = driver;
         next()
 
     } catch (error) {
