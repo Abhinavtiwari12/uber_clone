91a2ddf001053b28c0afb65aa5359a848fd6067a
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-11 23:01:39 +0530

    create driver login route

--- src/router/driver.router.js
+++ src/router/driver.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { registationForDriver } from "../controller/driver.controller.js";
+import { logingDriver, registationForDriver } from "../controller/driver.controller.js";
 
 
 const router = Router()
 
 
 router.route('/createDriver').post(registationForDriver)
+router.route('/loginDriver').post(logingDriver)
 
 
 export default router
