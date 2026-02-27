d717b976f2f695faa85bdf46fad0555600bf39cf
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-02-27 15:04:56 +0530

    make router for complete Ride, get Driver Ride History, get Driver Total Earnings, start Ride, toggle Driver Availability, update Driver Location APIs

--- src/router/driver.router.js
+++ src/router/driver.router.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { acceptRide, driverlogout, driverProfile, logingDriver, registationForDriver } from "../controller/driver.controller.js";
+import { 
+    acceptRide, 
+    completeRide, 
+    driverlogout, 
+    driverProfile, 
+    getDriverRideHistory, 
+    getDriverTotalEarnings, 
+    logingDriver, 
+    registationForDriver, 
+    startRide, 
+    toggleDriverAvailability, 
+    updateDriverLocation 
+} from "../controller/driver.controller.js";
 import { verifyDriverjwt } from "../middleware/autho.middleware.js";
 
 
@@ -11,6 +23,12 @@ router.route('/loginDriver').post(logingDriver)
 router.route('/driverProfile').get(verifyDriverjwt ,driverProfile)
 router.route('/driverLogout').post(verifyDriverjwt, driverlogout)
 router.route('/acceptRide').post(verifyDriverjwt, acceptRide)
+router.route('/startRide/:rideId').post(verifyDriverjwt, startRide)
+router.route('/completeRide/:rideId').post(verifyDriverjwt, completeRide)
+router.route('/updateDriverLocation').post(verifyDriverjwt, updateDriverLocation)
+router.route('/toggleDriverAvailability').post(verifyDriverjwt, toggleDriverAvailability)
+router.route('/getDriverTotalEarnings').get(verifyDriverjwt, getDriverTotalEarnings)
+router.route('/getDriverRideHistory').get(verifyDriverjwt, getDriverRideHistory)
 
 
 export default router
